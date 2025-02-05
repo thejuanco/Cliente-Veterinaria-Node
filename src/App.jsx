ab3c095deaede7f1b29ea36400f67caf3c53f342
Juanco <juanffo874@gmail.com>
2025-02-04 23:03:22 -0600

    Crea las paginas del area publica y las añade al routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Pagina maestra
 import AuthLayout from "./layout/AuthLayout"
+
+//Area publica
 import Login from "./pages/Login"
+import Register from "./pages/Register"
+import ConfirmAccount from "./pages/ConfirmAccount"
+import ForgotPassword from "./pages/ForgotPassword"
 
 function App() {
 
@@ -13,6 +20,9 @@ function App() {
       <Routes>
         <Route path="/" element={<AuthLayout/>}>
           <Route index element={<Login/>}/>
+          <Route path="registrar" element={<Register/>}/>
+          <Route path="olvide-password" element={<ForgotPassword/>}/>
+          <Route path="confirmar/:id" element={<ConfirmAccount/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -4,8 +4,7 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <>
-      <h1>Autenticación</h1>
-      <p>Esta es la pantalla de autenticación</p>
+      <h1>Administración de Pacientes de Veterinario</h1>
 
       <Outlet />
     </>
