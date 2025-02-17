55fdee5dc8ddfa4975867c38b3454b94fc4acc5e
Juanco <juanffo874@gmail.com>
2025-02-16 21:17:43 -0600

    Crea los componentes de footer y header y los agrega a la ruta

--- src/layout/ProtectedRoute.jsx
+++ src/layout/ProtectedRoute.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 const ProtectedRoute = () => {
 
     const { auth, loading } = useAuth();
-    console.log(auth)
-    console.log(loading)
 
     if(loading) return "Cargando...";
     
   return (
     <>
-        <div>
-            <h1>Esta es una ruta protegida</h1>
-        </div>
-        {auth?._id ? <Outlet/> : <Navigate to="/" />}
+        <Header/>
+            {auth?._id ? <Outlet/> : <Navigate to="/" />}
+        <Footer/>
+        
     </>
   )
 }
