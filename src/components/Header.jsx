8c741e03884d5f91c3fbf6eafd78d07793a0dd01
Juanco <juanffo874@gmail.com>
2025-02-16 21:37:08 -0600

    Agrega estilos al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <h1>Header</h1>
-  )
-}
+    <header className="py-8 bg-indigo-600">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="font-bold text-2xl mx-5 text-indigo-200">
+          Administrador de pacientes de {""}
+          <span className="text-white font-black">Veterinaria</span>
+        </h1>
+        <nav className="flex gap-4 mr-5 ">
+            <Link to="/admin" className="text-white text-lg font-bold p-2 rounded-lg hover:bg-indigo-500">Pacientes</Link>
+            <Link to="/admin" className="text-white text-lg font-bold px-3 py-2 rounded-lg hover:bg-indigo-500">Perfil</Link>
+            <button className="text-white text-sm font-bold px-3 rounded-full hover:bg-indigo-500" type="button">Cerrar Sesión</button>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
