cd2b991082b210b1b93d77f57dc276cbf1122f22
Juanco <juanffo874@gmail.com>
2025-02-16 22:43:55 -0600

    Crea el area de listado de pacientes

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+  return (
+    <div>ListadoPacientes</div>
+  )
+}
+
+export default ListadoPacientes
--- src/pages/AdminPatients.jsx
+++ src/pages/AdminPatients.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Formulario from '../components/Formulario'
+import ListadoPacientes from '../components/ListadoPacientes'
 
 const AdminPatients = () => {
   return (
-    <div>AdminPatients</div>
+    <div className='flex flex-col md:flex-row'>
+      <div className='md:w-1/2 lg:w-2/5'>
+        <Formulario/>
+      </div>
+      <div className='md:w-1/2 lg:w-3/5'>
+        <ListadoPacientes/>
+      </div>
+    </div>
   )
 }
 
