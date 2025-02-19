99b9a376c1ce64bcad6f4b1c16b282b09eac4e15
Juanco <juanffo874@gmail.com>
2025-02-18 22:01:27 -0600

    Hace resposivo el diseño del formulario

--- src/pages/AdminPatients.jsx
+++ src/pages/AdminPatients.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Formulario from '../components/Formulario'
 import ListadoPacientes from '../components/ListadoPacientes'
 
 const AdminPatients = () => {
+
+  const [mostrarFormulario, setMostrarFormulario] = useState(false)
+
   return (
     <div className='flex flex-col md:flex-row'>
-      <div className='md:w-1/2 lg:w-2/5'>
+      <button
+        type='button'
+        className='bg-indigo-600 text-white font-semibold mx-10 p-3 rounded-md mb-8 md:hidden'
+        onClick={() => setMostrarFormulario(!mostrarFormulario)}
+      >{mostrarFormulario ? "Ocultar formulario" : "Mostrar formulario"}</button>
+      <div className={`${mostrarFormulario ? "block" : "hidden" } md:block md:w-1/2 lg:w-2/5`}>
         <Formulario/>
       </div>
       <div className='md:w-1/2 lg:w-3/5'>
