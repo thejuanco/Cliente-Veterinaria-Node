045c998541ab87a9dc14eedc3ed889ce6718c6cd
Juanco <juanffo874@gmail.com>
2025-02-18 22:30:07 -0600

    Crea un hook para pacientes

--- src/context/PatientsProvider.jsx
+++ src/context/PatientsProvider.jsx
@@ -4,8 +4,13 @@ import clientAxios from "../config/Axios";
 const PatientsContext = createContext();
 
 export const PatientsProvider = ({ children }) => {
+
+    const [pacientes, setPacientes] = useState([])
+
     return(
-        <PatientsContext.Provider value={{}}>
+        <PatientsContext.Provider 
+            value={{pacientes}}
+        >
             {children}
         </PatientsContext.Provider>
     )
