3990df1e3fbae13791639da5eea0f0465599b8b0
Juanco <juanffo874@gmail.com>
2025-02-20 09:05:39 -0600

    Muestra de forma condicional si existen o no pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import usePatients from '../hooks/usePatients'
 
 const ListadoPacientes = () => {
+  const {pacientes} = usePatients();
+  
+
   return (
-    <div>ListadoPacientes</div>
+    <>
+      {pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">Administra tus pacientes {''}<span className="text-indigo-600 font-bold">y citas</span></p>
+        
+        </>
+      ): (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">Comienza a agregar pacientes {''}<span className="text-indigo-600 font-bold">y apareceran aquí</span></p>
+        </>
+      )}
+    </>
   )
 }
 
