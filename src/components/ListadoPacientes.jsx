0f1a19503654d86cb42f0d68442bd332a73569e3
Juanco <juanffo874@gmail.com>
2025-02-20 17:24:38 -0600

    Crea el componente para listar los pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import usePatients from '../hooks/usePatients'
+import Paciente from './Paciente';
 
 const ListadoPacientes = () => {
   const {pacientes} = usePatients();
@@ -11,7 +12,12 @@ const ListadoPacientes = () => {
         <>
           <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">Administra tus pacientes {''}<span className="text-indigo-600 font-bold">y citas</span></p>
-        
+          {pacientes.map(paciente => (
+            <Paciente 
+              key={paciente._id}
+              paciente={paciente}
+            />
+          ))}
         </>
       ): (
         <>
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Paciente = ({paciente}) => {
+    const {email, fecha, nombre, propietario, sintomas, _id} = paciente
+
+    function formatearFecha(fecha){
+        const nuevaFecha = new Date(fecha)
+
+        return new Intl.DateTimeFormat("es-MX", {dateStyle: 'long'}).format(nuevaFecha)
+    }
+
+  return (
+    <div className="mx-5 my-10 bg-white shadow-md py-10 px-5 rounded-xl">
+      <p className="font-bold uppercase text-indigo-800 my-2">
+        Nombre: {""}
+        <span className="font-normal normal-case text-black">{nombre}</span>
+      </p>
+      <p className="font-bold uppercase text-indigo-800 my-2">
+        Propietario: {""}
+        <span className="font-normal normal-case text-black">
+          {propietario}
+        </span>
+      </p>
+      <p className="font-bold uppercase text-indigo-800 my-2">
+        Correo electronico: {""}
+        <span className="font-normal normal-case text-black">{email}</span>
+      </p>
+      <p className="font-bold uppercase text-indigo-800 my-2">
+        Fecha de alta: {""}
+        <span className="font-normal normal-case text-black">
+          {formatearFecha(fecha)}
+        </span>
+      </p>
+      <p className="font-bold uppercase text-indigo-800 my-2">
+        Sintomas: {""}
+        <span className="font-normal normal-case text-black">{sintomas}</span>
+      </p>
+
+      <div className="flex justify-between my-8">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg"
+        >
+          Editar
+        </button>
+
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg"
+        >
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Paciente
