f898f087d996f61f585136116424d03ae21cba40
Juanco <juanffo874@gmail.com>
2025-02-20 20:08:07 -0600

    Añade información del paciente al formulario

--- src/pages/EditPerfil.jsx
+++ src/pages/EditPerfil.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import AdminNav from "../components/AdminNav";
+import useAuth from "../hooks/useAuth";
 
 const EditPerfil = () => {
+
+  const {auth} = useAuth();
+  const [perfil, setPerfil] = useState({});
+
+  useEffect(() => {
+    setPerfil(auth);
+  }, [auth])
+  
   return (
     <>
       <AdminNav />
@@ -22,6 +31,11 @@ const EditPerfil = () => {
                 type="text"
                 className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
                 name="nombre"
+                value={perfil.nombre || ""}
+                onChange={e => setPerfil({
+                  ...perfil,
+                  [e.target.name] : e.target.value
+                })}
               />
             </div>
 
