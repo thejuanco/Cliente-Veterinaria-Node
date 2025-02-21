f4c51ced6950ddd07226a5108f927bd79bd2bb63
Juanco <juanffo874@gmail.com>
2025-02-20 22:05:53 -0600

    Crea el formulario para restablecer la contraseña

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import AdminNav from "../components/AdminNav";
+import Alert from "../components/Alert";
 
 const ChangePassword = () => {
+  const [alerta, setAlerta] = useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
   return (
     <>
       <AdminNav />
@@ -12,6 +20,51 @@ const ChangePassword = () => {
         Cambia tu {""}
         <span className="text-indigo-600 font-bold">contraseña aqui</span>
       </p>
+      <div className="flex justify-center">
+        <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
+          <form onSubmit={handleSubmit}>
+            {alerta.msg && <Alert alerta={alerta} />}
+
+            <div className="my-3">
+              <label className="uppercase font-semibold text-gray-600">
+                Contraseña actual
+              </label>
+              <input
+                type="text"
+                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                name="sitioWeb"
+                placeholder="Escribe tu contraseña actual"
+                // value={perfil.sitioWeb || ""}
+                // onChange={(e) =>
+                //   setPerfil({
+                //     ...perfil,
+                //     [e.target.name]: e.target.value,
+                //   })
+                // }
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-semibold text-gray-600">
+                Nueva contraseña
+              </label>
+              <input
+                type="text"
+                className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                name="sitioWeb"
+                placeholder="Escribe tu nueva contraseña"
+                
+              />
+            </div>
+
+            <input
+              type="submit"
+              value="Actualizar contraseña"
+              className="bg-indigo-700 px-10 py-3 font-semibold text-white rounded-lg uppercase w-full mt-5"
+            />
+          </form>
+        </div>
+      </div>
     </>
   );
 };
